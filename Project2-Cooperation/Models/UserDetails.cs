9ec94b633141c865db558eb57f52ff81f81c1bca
cpanousakis <33422876+cpanousakis@users.noreply.github.com>
2018-02-01 14:05:27 +0200

    Validations OrderForm
    All the changes are commented

--- Project2-Cooperation/Models/UserDetails.cs
+++ Project2-Cooperation/Models/UserDetails.cs
@@ -10,19 +10,38 @@ namespace Project2_Cooperation.Models
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
 
-        [Required]
-        [Display(Name ="First Name")]
+
+        //First Name,Last Name, A-Z,a-z, 3-40 Characters
+        //Phone 10 digits
+        //Adress Letters 3-40 & Digits 1-3
+        //Postal Code 5 Digits
+        [Required(ErrorMessage = "Name is Required!")]
+        //[RegularExpression(@"^[a-zA-Z''-'\s]{3,40}$", ErrorMessage = "First Name is not valid.")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Name is Required!")]
+        // [RegularExpression(@"^[a-zA-Z''-'\s]{3,40}$", ErrorMessage = "Last Name is not valid.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required]
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Phone Number Required!")]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
         public string Phone { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "City is Required!")]
+        //[RegularExpression(@"^[a-zA-Z''-'\s]{2,40}$", ErrorMessage = "City is not valid.")]
         public string City { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Adress is Required!")]
+        [DataType(DataType.MultilineText)]
+        //[RegularExpression(@"^[a-zA-Z''-'\s]{2,40}\d{1,3}$", ErrorMessage = "Adress is not valid.")]
         public string Address { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Zip code is Required!")]
+        [DataType(DataType.PostalCode)]
+        // [RegularExpression(@"^\(?([0-4]{3})\)?[-. ]?([0-4]{2})[-. ]?([0-4]{0})$", ErrorMessage = "Entered zip code is not valid.")]
         [Display(Name = "Postal Code")]
         public string Zip { get; set; }
     }
